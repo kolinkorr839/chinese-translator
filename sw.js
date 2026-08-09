88560c96201ceacdd427636f675e1094d3062e27
kolinkorr839 <bichonfrise74@gmail.com>
2026-08-09 15:31:49 -0700

    Initial PWA

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'chinese-translator-v1';
+const STATIC_ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.jsdelivr.net/npm/pinyin-pro@3/dist/index.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  if (url.hostname === 'translate.googleapis.com' || url.hostname === 'translate.google.com') {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
